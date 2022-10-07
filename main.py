dff2de014248c6cb02b71acb40b971c35c4d730a
Hasan <hasanumutuzunoz@gmail.com>
2022-10-06 18:52:18 -0700

    Snooze Alarm added

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import sys
-import datetime
 import winsound
+from datetime import datetime, timedelta, time
 from threading import *
 from keyboard import press
 
 
-date_time = datetime.datetime.today()
-print("Current Date : {}/{}/{}".format(date_time.day, date_time.month, date_time.year))
-print("Current Time : {}:{}".format(date_time.hour, date_time.minute))
-alarmHour = int(input("Enter alarm hour"))
-alarmMinute = int(input("Enter alarm minute"))
+dt = datetime.now().replace(microsecond=0)
+print("Current Date : {}/{}/{}".format(dt.day, dt.month, dt.year))
+print("Current Time : {}:{}".format(dt.hour, dt.minute))
+alarmHour, alarmMinute = [int(x) for x in input("Enter alarm hour and minute separated by : (e.g. 4:11) ").split(':')]
+alarmTime = datetime(dt.year, dt.month, dt.day, alarmHour, alarmMinute, 0)
+print("Alarm Time: ", alarmTime)
 alarmStop = False
 musicFile = 'C:/Users/hasan/PycharmProjects/pythonMiniProjects/alarmClock/snowRabbit.wav'
+run_once = 1
 
 
 def play():
-    run_once = 1
     while True:
         # Update date time every second
-        dt = datetime.datetime.today()
+        global dt, run_once, alarmTime
+        dt = datetime.now().replace(microsecond=0)
 
         # Start the alarm at alarm hour and alarm minute
         # Run this code once to prevent music error
-        if run_once == 1 and dt.hour == alarmHour and dt.minute == alarmMinute:
+        if run_once == 1 and dt == alarmTime:
             winsound.PlaySound(musicFile, winsound.SND_ASYNC)
             run_once = -1
             # Run the stop alarm thread at the same time.
@@ -30,7 +32,7 @@ def play():
             Thread(target=stop).start()
 
         # Stop the alarm after 1 minute, Stop thread 2
-        elif dt.hour == alarmHour and dt.minute == alarmMinute + 1:
+        elif dt == alarmTime + timedelta(minutes=1):
             winsound.PlaySound(None, winsound.SND_PURGE)
             # To stop thread 2, press enter while alarmStop True
             print("\nAlarm stopped after 1 minute")
@@ -45,23 +47,25 @@ def play():
 
 # Stop the music if input is 'STOP'
 def stop():
+    global dt, run_once, alarmTime, alarmStop
     while not alarmStop:
-        user_input_ref = str(input("To stop the alarm, type 'STOP'"))
-        if user_input_ref == "STOP":
+        user_input_ref = input("\nTo stop the alarm, type 'STOP'"
+                               "\nTo snooze the alarm, enter minute ")
+        # Snooze if the input is int
+        if user_input_ref.strip().isdigit():
+            winsound.PlaySound(None, winsound.SND_PURGE)
+            user_input_ref = float(user_input_ref)
+            alarmTime = dt + timedelta(minutes=user_input_ref)
+            run_once = 1
+            print("Alarm time: ", alarmTime)
+            break
+
+        # Stop if the input is 'STOP'
+        elif user_input_ref == "STOP":
             winsound.PlaySound(None, winsound.SND_PURGE)
             globals()["alarmStop"] = True
             sys.exit()
-        else:
-            stop()
 
 
 if __name__ == '__main__':
     Thread(target=play).start()
-
-
-
-
-
-
-
-
